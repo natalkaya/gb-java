e438f878af4df0f7c5b9ce6a541771973f901230
Natalia Poiarkova <npoiarkova@natera.com>
2020-10-11 22:31:58 +0300

    + added lesson7: chat(I part)

---
+++ src/main/java/lesson7/client/Main.java
@@ -0,0 +1,27 @@
+package lesson7.client;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.net.URL;
+import java.util.Objects;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("../../sample.fxml")));
+        primaryStage.setTitle("Балабол");
+        primaryStage.setScene(new Scene(root, 400, 300));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/main/java/lesson7/server/Server.java
@@ -0,0 +1,84 @@
+package lesson7.server;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class Server {
+    private List<ClientHandler> clients;
+    private AuthService authService;
+
+    public Server() {
+        clients = new CopyOnWriteArrayList<>();
+        authService = new SimpleAuthService();
+        ServerSocket server = null;
+        Socket socket = null;
+        final int PORT = 8189;
+
+        try {
+            server = new ServerSocket(PORT);
+            System.out.println("Server started");
+
+            while (true) {
+                socket = server.accept();
+                new ClientHandler(this, socket);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                server.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void broadcastMsg(ClientHandler sender, String msg) {
+        String message = String.format("[ %s ]: %s", sender.getNickname(), msg);
+        for (ClientHandler c : clients) {
+            c.sendMsg(message);
+        }
+    }
+
+    public void privateMsg(ClientHandler sender, String msg) {
+        if (msg.startsWith("/w ")) {
+            String[] arr = msg.trim().split(" ");
+            String recipientNick = arr[1];
+            String message = String.format("[ %s ]: %s", sender.getNickname(), arr[2]);
+
+            Optional<ClientHandler> recipient = clients.stream()
+                    .filter(c -> c.getNickname().equals(recipientNick))
+                    .findFirst();
+
+            if (recipient.isPresent()) {
+                sender.sendMsg(message);
+                recipient.get().sendMsg(message);
+            } else {
+                sender.sendMsg("User is offline / not found");
+            }
+        }
+
+    }
+
+    public void subscribe(ClientHandler clientHandler) {
+        clients.add(clientHandler);
+    }
+
+    public void unsubscribe(ClientHandler clientHandler) {
+        clients.remove(clientHandler);
+    }
+
+    public AuthService getAuthService() {
+        return authService;
+    }
+}
